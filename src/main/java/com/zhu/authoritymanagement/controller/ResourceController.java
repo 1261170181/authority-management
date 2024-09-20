cda12d9bfb09a49e8a8a271864739be508550e9a
Zhu <1261170181@qq.com>
2024-09-20 16:43:18 +0800

    添加控制器判断条件

--- src/main/java/com/zhu/authoritymanagement/controller/ResourceController.java
+++ src/main/java/com/zhu/authoritymanagement/controller/ResourceController.java
@@ -2,14 +2,12 @@ package com.zhu.authoritymanagement.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.zhu.authoritymanagement.entity.Account;
 import com.zhu.authoritymanagement.entity.Resource;
 import com.zhu.authoritymanagement.service.IResourceService;
 import com.zhu.authoritymanagement.util.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
